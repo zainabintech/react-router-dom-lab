890588caf52f0da2f25c0caba3116714f3e54e10
zainab <zainabhammaad@gmail.com>
2025-02-17 11:19:33 +0300

    k

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
